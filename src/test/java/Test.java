76f96c930ed589f2bd5a1aec59669a2109b82e50
emilienrestoueix <restouee@3il.fr>
2025-10-15 12:48:14 +0200

    update pom to add checkstyle, jacoco and spotbug

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+
+public class Test {
+
+    // test mise en place jacoco
+    @org.junit.Test
+    public void Test1(){
+        int a = 1;
+
+        Assert.assertEquals(1, a);
+    }
+}
